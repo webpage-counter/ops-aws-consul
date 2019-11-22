57ff7ca6b557345f0f50c20528e89524da968586
Denislav Denov <denislavdenov@gmail.com>
2019-11-22 09:01:37 +0200

    adding code

---
+++ main.tf
@@ -0,0 +1,53 @@
+data "terraform_remote_state" "nw" {
+  backend = "remote"
+
+  config = {
+    organization = "webpage-counter"
+    workspaces = {
+      name = "ops-aws-network"
+    }
+    token = var.token
+  }
+}
+
+module "dc1" {
+  source = "app.terraform.io/webpage-counter/consul/aws"
+  version = "0.0.1"
+  instance_type     = var.instance_type
+  region            = var.region
+  dcname            = var.dcname["dc1"]
+  IP                = var.IP_dc1
+  join_wan          = var.join_wan
+  token             = var.token
+}
+
+output "ami" {
+  value = module.dc1.ami
+}
+
+output "instance_type" {
+  value = module.dc1.instance_type
+}
+
+
+output "dcname" {
+  value = module.dc1.dcname
+}
+
+output "IP_client" {
+  value = module.dc1.IP_client
+}
+
+
+output "data_rendered" {
+  value = module.dc1.data_rendered
+}
+
+output "instance_profile" {
+  value = module.dc1.iam_instance_profile
+}
+
+output "key_pair" {
+  value = module.dc1.key_pair
+}
+
